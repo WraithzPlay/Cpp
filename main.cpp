5a6c51c61725df0e79ae78753186ad44e4c84b5a
WraithzPlay <wraithzplay@gmail.com>
2020-11-04 06:31:12 +0800

    Setting UP

---
+++ main.cpp
@@ -0,0 +1,39 @@
+/*
+* AUTHOR : WraithzPlay
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pi 3.1415926535897932384626
+#define sci(x) scanf("%d", &x)
+#define scl(x) scanf("%lld", &x)
+#define scs(s) scanf("%s", s)
+#define pri(x) printf("%d\n", x)
+#define prl(x) printf("%lld\n", x)
+#define prs(s) printf("%s\n", s)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define sorted(x) sort(x.begin(),x.end())
+#define FOR(n) for(int i = 0;i < n;i++)
+
+typedef pair<int,int> pii;
+typedef pair<long,long> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vii;
+
+const int N = int(1e5 + 3);
+
+/* The Solution */
+void solution(){
+    
+}
+
+int main(){
+    int t = 1;
+    while(t--) solution();
+}
+
