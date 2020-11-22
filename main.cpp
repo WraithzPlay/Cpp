e909921e9e96566f53e6f5a023534f2c09afb22f
WraithzPlay <70443188+WraithzPlay@users.noreply.github.com>
2020-11-22 08:30:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,39 @@
-/*
-* AUTHOR : WraithzPlay
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define pi 3.1415926535897932384626
-#define sci(x) scanf("%d", &x)
-#define scl(x) scanf("%lld", &x)
-#define scs(s) scanf("%s", s)
-#define pri(x) printf("%d\n", x)
-#define prl(x) printf("%lld\n", x)
-#define prs(s) printf("%s\n", s)
+#define scd(v) scanf("%d",&v)
+#define scld(v) scanf("%ld",&v)
+#define sclld(v) scanf("%lld",&v)
+#define scs(v) scanf("%s",&v)
+#define sclf(v) scanf("%lf",&v)
+
+#define rep(i, j) for(int i = 0;i < j;i++)
+#define all(v) v.begin(),v.end()
+#define cnt(v,n) count(ALL(v),n)
+#define sz(v) v.size()
+#define fi first
+#define se second
 #define pb push_back
 #define mp make_pair
-#define all(x) x.begin(),x.end()
-#define sorted(x) sort(x.begin(),x.end())
-#define FOR(n) for(int i = 0;i < n;i++)
+#define inf int(1e9)
+#define ll long long
+#define pi 3.14159265358979
 
-typedef pair<int,int> pii;
-typedef pair<long,long> pl;
+typedef pair<int, int> pii;
 typedef vector<int> vi;
-typedef vector<ll> vl;
 typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
 
-const int N = int(1e5 + 3);
-
-/* The Solution */
-void solution(){
-    
+void solve(){
 }
 
 int main(){
-    int t = 1;
-    while(t--) solution();
+    ios_base::sync_with_stdio(false),cin.tie(0);
+    int t=1;
+    scd(t);
+    while(t--) solve();
+    return 0;
 }
 
